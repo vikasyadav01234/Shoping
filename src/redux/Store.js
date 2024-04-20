5930a402db073a76f6c363390565a0fd81a62cba
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-20 12:51:40 +0530

    main

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { CartSlice } from "./Slices/CartSlice";
+import { CartSlice } from "./Slices/cartSlice";
 
 
 export const store = configureStore({
