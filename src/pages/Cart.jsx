7d32998422861265c88b12dcf1f62632fa699cfa
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-20 22:58:56 +0530

    main

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -9,7 +9,11 @@ const Cart = () => {
         ():
         (<div>
           <h1>Cart Empty</h1>
-          <Link to={"/"}/>
+          <Link to={"/"}>
+            <button>
+              Shop Now
+            </button>
+          </Link>
         </div>)
       }
     </div>
