fe023c10ddfcfd1e2d5eba39b9c91719bbb57baf
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-20 12:14:31 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Cart from "./pages/Cart"
 const App = () => {
   return (
     <div>
