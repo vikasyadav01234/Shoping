84463477180687ba0778c1a3e57587777e01d98c
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-20 12:16:23 +0530

    main

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/Store";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Provider store={store}>
+
+  <BrowserRouter>
+    
+    <Provider store={store}>
   
-    <App />
-  </Provider>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
