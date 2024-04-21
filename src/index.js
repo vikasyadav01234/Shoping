37a3da07a4288ea4c6689d9237f2f50a257bf51a
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-21 12:29:24 +0530

    main

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/Store";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -12,9 +13,11 @@ root.render(
 
   <BrowserRouter>
     
+    <ToastContainer>
     <Provider store={store}>
   
       <App />
     </Provider>
+    </ToastContainer>
   </BrowserRouter>
 );
