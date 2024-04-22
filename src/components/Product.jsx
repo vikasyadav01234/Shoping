b64b576c567b1170abb2e70679862d4382beceff
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-22 09:11:20 +0530

    main

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -37,7 +37,7 @@ const Product = ({post}) => {
         {
           cart.some((p)=> p.id === post.id) ?
           (<button
-          className="text-gray-700 border-2 border-gray-700 rounded-full"
+          className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold"
           onClick={removeFromCart}
           >
             Remove Item
