ac50a837f46e6d4658e52dff83fc32c13a5b290a
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-22 08:09:28 +0530

    main

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -15,7 +15,7 @@ const Product = ({post}) => {
   }
   return (
     <div className="flex flex-col items-center justify-between
-    hover:scale-110 transition-all duration-300 ease-in">
+    hover:scale-110 transition-all duration-300 ease-in shadow-[rgba(6,_24,_44,_0.4)_0px_0px_0px_2px,_rgba(6,_24,_44,_0.65)_0px_4px_6px_-1px,_rgba(255,_255,_255,_0.08)_0px_1px_0px_inset]">
       <div>
         <p>{post.title}</p>
       </div>
