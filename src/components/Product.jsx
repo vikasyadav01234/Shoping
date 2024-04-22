bb2c03f4e8708b4ba613471025d39fd97b5e2d3e
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-22 09:15:38 +0530

    main

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -38,7 +38,7 @@ const Product = ({post}) => {
           cart.some((p)=> p.id === post.id) ?
           (<button
           className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold text-[12px]
-          p-1 px-3 uppercase"
+          p-1 px-3 uppercase hover:bg-gray-700 hover:text-white"
           onClick={removeFromCart}
           >
             Remove Item
