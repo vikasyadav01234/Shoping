f54c2f17408c5b1e85ddebf167e3dfc07fad3d22
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-23 11:58:29 +0530

    main

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -33,7 +33,7 @@ const Cart = () => {
             </div>
             <div className="flex flex-col">
               <p className="text-xl font-bold"><span className="text-gray-700 font-semibold">Total Amount :</span>${totalAmount}</p>
-              <button>
+              <button className="bg-green-700 hover:bg-purple-50 rounded-lg text-white transition duration-300 ease-linear mt-5 border-2 border-green-600 font-bold hover:text-green-700 p-3 text-xl">
                 CheckOut Now
               </button>
             </div>
