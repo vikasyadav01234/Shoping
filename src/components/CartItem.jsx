6adb9095ec2f284e848d189d02bfb736f8e2e175
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-23 11:43:49 +0530

    main

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -20,7 +20,8 @@ const CartItem = ({item, itemIndex}) => {
           <h1 className="text-base text-slate-700 font-medium">{item.description}</h1>
           <div className="flex items-center justify-between">
             <p className="font-bold text-lg text-green-600">{item.price}</p>
-            <div onClick={removeFromCart}>
+            <div className="text-red-800  bg-red-200 group hover:bg-red-400 transition-transform duration-300 cursor-pointer rounded-full p-3 mr-3" 
+            onClick={removeFromCart}>
               <FcDeleteDatabase/>
             </div>
           </div>
