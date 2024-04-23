52544a0a052fc42e9eab03c8448df19803270ae7
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-23 11:53:55 +0530

    main

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -24,7 +24,7 @@ const Cart = () => {
           <div className="w-[100%] md:w-[40%] mt-5  flex flex-col">
             <div className="flex flex-col p-5 gap-5 my-14  h-[100%] justify-between">
               <div className="flex flex-col gap-5 ">
-              <div>Your Cart</div>
+              <div className="font-semibold text-xl text-green-800 ">Your Cart</div>
               <div>Summary</div>
               <p>
                 <span>Total Items : {cart.length}</span>
