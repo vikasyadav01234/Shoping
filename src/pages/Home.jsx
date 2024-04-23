57ef40e4adcfa4d8c8dcc837438ea6f63db11486
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-23 12:05:55 +0530

    main

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
       {
         loading ? <Spinner/> : 
         posts.length > 0 ?
-        (<div className="grid grid-cols-4 xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
+        (<div className="grid  xs:gridcols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
           {
             posts.map((post)=>(
             <Product key={posts.id} post={post}/>
